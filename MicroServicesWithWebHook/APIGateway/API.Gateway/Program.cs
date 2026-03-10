ec084de158b4cdb145a98cd1ca778df7c146b083
rahul <rahulsalinskee@gmail.com>
2026-03-10 22:32:00 +0530

    Added NuGET package for Gateway. Updated AppSettings.json Configure in MiddleWare

--- MicroServicesWithWebHook/APIGateway/API.Gateway/Program.cs
+++ MicroServicesWithWebHook/APIGateway/API.Gateway/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+/* Add YARP Reverse Proxy Services */
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -15,6 +18,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+/* Map YARP Middleware */
+app.MapReverseProxy();
 
 app.UseAuthorization();
 
